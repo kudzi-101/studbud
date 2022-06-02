52bc46dfd790b038d00caff6401bb3c29457c866
kudzi-101 <dorothydube02@gmail.com>
2022-06-02 14:00:18 +1000

    Creating pomodoro timer html and JS functions

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -57,5 +57,71 @@ function resetStopwatch() {
 startBttn.addEventListener('click', startStopwatch);
 stopBttn.addEventListener('click', stopStopwatch);
 resettBttn.addEventListener('click', resetStopwatch);
+/* POMODORO TIMER */ let minString = document.getElementById('min');
+let secString = document.getElementById('sec');
+let startButton = document.getElementById('startButton');
+localStorage.setItem("btn", "session");
+let initial, totalsecs, perc, paused, mins, seconds;
+startBttn.addEventListener("click", ()=>{
+    let bttn = localStorage.getItem("bttn");
+    if (bttn === "session") mins = +localStorage.getItem("sessionTime") || 1;
+    else mins = +localStorage.getItem("breakTime") || 1;
+    seconds = mins * 60;
+    totalsecs = mins * 60;
+    setTimeout(decremenT(), 60);
+    startBttn.style.transform = "scale(0)";
+    paused = false;
+});
+function decremenT() {
+    minString.textContent = Math.floor(seconds / 60);
+    secString.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+}
+if (seconds > 0) {
+    seconds--;
+    initial = window.setTimeout("decremenT()", 1000);
+} else {
+    mins = 0;
+    seconds = 0;
+    let bttn = localStorage.getItem("bttn");
+    if (bttn === "session") {
+        startBttn.textContent = "start break";
+        startBttn.classList.add("break");
+        localStorage.setItem("bttn", "break");
+    } else {
+        startBttn.classList.remove("break");
+        startBttn.textContent = "start session";
+        localStorage.setItem("bttn", "session");
+    }
+    const sessionTimeInput = document.querySelector("#sessionTime");
+    const breakTimeInput = document.querySelector("#breakTime");
+    const stopBttn = document.querySelector(".pause");
+    sessionTimeInput.value = localStorage.getItem("sessionTime");
+    breakTimeInput.value = localStorage.getItem("breakTime");
+    document.querySelector("form").addEventListener("submit", (e)=>{
+        e.preventDefault();
+        localStorage.setItem("sessionTime", focusTimeInput.value);
+        localStorage.setItem("breakTime", breakTimeInput.value);
+    });
+    document.querySelector(".reset").addEventListener("click", ()=>{
+        startBttn.style.transform = "scale(1)";
+        clearTimeout(initial);
+        minString.textContent = 0;
+        secString.textContent = 0;
+    });
+    stopBttn.addEventListener("click", ()=>{
+        if (paused === undefined) return;
+        if (paused) {
+            paused = false;
+            initial = setTimeout("decremenT()", 60);
+            stopBttn.textContent = "stop";
+            stopBttn.classList.remove("resume");
+        } else {
+            clearTimeout(initial);
+            stopBttn.textContent = "resume";
+            stopBttn.classList.add("resume");
+            paused = true;
+        }
+    });
+}
 
 //# sourceMappingURL=index.f3bd186e.js.map
