756d0a365af8e36ac1a66a243f9b99ffc021b552
kudzi-101 <dorothydube02@gmail.com>
2022-06-04 02:12:00 +1000

    Created pop out formand javascript functions to make new task cards

--- public/kanban.js
+++ public/kanban.js
@@ -42,3 +42,69 @@ function dragLeave(){
 function dragDrop(){
     this.appendChild(draggableTask)
 }
+
+
+
+const btns = document.querySelectorAll("[data-target-modal]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.targetModal).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
+
+const taskSubmit = document.getElementById("taskSubmit");
+taskSubmit.addEventListener('click', createTask );
+
+function createTask (s) {
+    const taskDiv= document.createElement("div");
+    const inputValue = document.getElementById("taskInput").value;
+    const text = document.createTextNode(inputValue);
+    
+    taskDiv.appendChild(text);
+    taskDiv.classList.add("task");
+    taskDiv.setAttribute("draggable", "true");
+
+    const i= document.createElement("i");
+    const icon= document.createTextNode("fa-solid fa-xmark");
+    i.classList.add("close");
+    i.appendChild(icon);
+    document.body.appendChild(i)
+
+    taskDiv.appendChild(i);
+
+    noStatus.appendChild(taskDiv);
+
+    taskForm.classList.remove("active");
+    overlay.classList.remove("active");
+
+    taskDiv.addEventListener("dragstart", dragStart);
+    taskDiv.addEventListener("dragend", dragEnd);
+}
+
+const close_btns = document.querySelectorAll(".close");
+
+close_btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.style.display = "none";
+  });
+});
